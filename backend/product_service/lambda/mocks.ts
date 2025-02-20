0d9128e3adaac7442dab55a0ed502a2d169a98e1
Rodion Grishaev <r.grishaev@softteco.com>
2025-02-20 15:33:44 +0300

    fix: add mocks to a separate file.

---
+++ backend/product_service/lambda/mocks.ts
@@ -0,0 +1,23 @@
+export const products = [
+  {
+    id: 'product_1',
+    title: 'Product 1',
+    description: 'This is the first product',
+    count: 10,
+    price: 100,
+  },
+  {
+    id: 'product_2',
+    title: 'Product 2',
+    description: 'This is the second product',
+    count: 20,
+    price: 200,
+  },
+  {
+    id: 'product_3',
+    title: 'Product 3',
+    description: 'This is the third product',
+    count: 30,
+    price: 300,
+  },
+];
--- backend/product_service/lambda/product_list.ts
+++ backend/product_service/lambda/product_list.ts
@@ -1,26 +1,4 @@
-const products = [
-  {
-    id: 'product_1',
-    title: 'Product 1',
-    description: 'This is the first product',
-    count: 10,
-    price: 100,
-  },
-  {
-    id: 'product_2',
-    title: 'Product 2',
-    description: 'This is the second product',
-    count: 20,
-    price: 200,
-  },
-  {
-    id: 'product_3',
-    title: 'Product 3',
-    description: 'This is the third product',
-    count: 30,
-    price: 300,
-  },
-];
+import { products } from './mocks'
 
 const headers = {
   "Content-Type": "application/json",
