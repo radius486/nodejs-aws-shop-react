cfb77b2b5be29dd550df1e6b6220afffbd23b338
Rodion Grishaev <r.grishaev@softteco.com>
2025-02-28 14:29:18 +0300

    feat: grant permissions to lambda functions.

--- backend/product_service/lib/product_service-stack.ts
+++ backend/product_service/lib/product_service-stack.ts
@@ -2,10 +2,23 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 
 export class ProductServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const productsTable = dynamodb.Table.fromTableName(
+      this,
+      'ProductsTable',
+      'products'
+    );
+
+    const stocksTable = dynamodb.Table.fromTableName(
+      this,
+      'StocksTable',
+      'stocks'
+    );
 
     const productListFunction = new lambda.Function(this, 'ProductListFunction', {
       runtime: lambda.Runtime.NODEJS_20_X,
@@ -18,6 +31,13 @@ export class ProductServiceStack extends cdk.Stack {
       code: lambda.Code.fromAsset('dist/lambda'),
       handler: 'product_by_id.handler',
     });
+
+    productsTable.grantReadData(productListFunction);
+    stocksTable.grantReadData(productListFunction);
+    productsTable.grantReadData(productByIdFunction);
+    stocksTable.grantReadData(productByIdFunction);
+    // productsTable.grantWriteData(productCreateFunction);
+    // stocksTable.grantWriteData(productCreateFunction);
 
     const productApi = new apigateway.LambdaRestApi(this, 'ProductApi', {
       handler: productListFunction,
