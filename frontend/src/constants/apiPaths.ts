ea56e9c471ef1de19b08a8bbfccd94f9ae7bd6d2
Rodion Grishaev <r.grishaev@softteco.com>
2025-04-12 21:32:40 +0300

    feat: update API endpoints paths.

--- frontend/src/constants/apiPaths.ts
+++ frontend/src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://hq21lip4pj.execute-api.eu-west-1.amazonaws.com/prod",
-  order: "http://radius-aws-cart-api.eu-west-1.elasticbeanstalk.com/api/profile/cart",
+  order: "http://radius486-cart-api-develop.eu-west-1.elasticbeanstalk.com/api/profile/cart",
   import: "https://xpey3y7d1c.execute-api.eu-west-1.amazonaws.com/prod",
   bff: "https://hq21lip4pj.execute-api.eu-west-1.amazonaws.com/prod",
-  cart: "http://radius-aws-cart-api.eu-west-1.elasticbeanstalk.com/api",
+  cart: "http://radius486-cart-api-develop.eu-west-1.elasticbeanstalk.com/api",
 };
 
 export default API_PATHS;
