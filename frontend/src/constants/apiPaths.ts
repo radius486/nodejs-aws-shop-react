155fcafdfeb9f3d5bb80b85551d66bb28cc6c754
Rodion Grishaev <r.grishaev@softteco.com>
2025-04-12 19:28:15 +0300

    feat: apdate endpoints to use API from EB instanses.

--- frontend/src/constants/apiPaths.ts
+++ frontend/src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://hq21lip4pj.execute-api.eu-west-1.amazonaws.com/prod",
-  order: "https://yafjcuzag6.execute-api.eu-west-1.amazonaws.com/prod/api/profile/cart",
+  order: "http://radius-aws-cart-api.eu-west-1.elasticbeanstalk.com/api/profile/cart",
   import: "https://xpey3y7d1c.execute-api.eu-west-1.amazonaws.com/prod",
   bff: "https://hq21lip4pj.execute-api.eu-west-1.amazonaws.com/prod",
-  cart: "https://yafjcuzag6.execute-api.eu-west-1.amazonaws.com/prod/api",
+  cart: "http://radius-aws-cart-api.eu-west-1.elasticbeanstalk.com/api",
 };
 
 export default API_PATHS;
